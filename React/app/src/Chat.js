a7914be873f66fd1a4b88cfd867613c0d761d740
PatoSala <patosala998@gmail.com>
2021-06-04 16:37:50 -0300

    ubicando elementos del front

--- React/app/src/Chat.js
+++ React/app/src/Chat.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from "react";
+import './Chat.css';
+
 const Chat = ({selectedChat}) => {
-    const url = "http://localhost:3000/api/getmessages/" + selectedChat;
+    const url = "http://localhost:3000/api/getmessages/" + /* selectedChat */ 5491132296692;
 
     const [state, setState] = useState({messages: undefined})
     const getData = async () => {
@@ -22,8 +24,16 @@ const Chat = ({selectedChat}) => {
                 <div className="messages-wrapper">
                     <ul>
                         {state.messages.map(message => {
+                            if (message.hasMedia) {
+                                message.body = 'Media Message';
+                            }
+                            if (message.fromMe) {
+                                return (
+                                    <li className="fromMe">{message.body}</li>
+                                )
+                            }
                             return (
-                                <li>{message.body}</li>
+                                <li className="fromElse">{message.body}</li>
                             )
                         })}
                     </ul>
